0a6ccbbb69679420406ea7c553f1e2c0fb85f581
titayi <kitayimike@gmail.com>
2023-02-11 12:50:39 -0800

    Updates

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-"""Creates a unique FileStorage instance for the application
-"""
+""" unique FileStorage instance"""
+
 from models.engine.file_storage import FileStorage
+
+classes = {'BaseModel': 'BaseModel', 'Amenity': 'Amenity', 'State': 'State',
+           'Place': 'Place', 'Review': 'Review', 'User': 'User'}
+
 storage = FileStorage()
 storage.reload()
